6e1c03297d44c26b5c493de1456b5759317045a0
Matheus Jorge <mrljorge@outlook.com>
2024-05-14 17:16:57 -0300

    Initial Commit

---
+++ Top10/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Top10
+//
+//  Created by Matheus Jorge on 5/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Top10/Top10App.swift
@@ -0,0 +1,17 @@
+//
+//  Top10App.swift
+//  Top10
+//
+//  Created by Matheus Jorge on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Top10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
