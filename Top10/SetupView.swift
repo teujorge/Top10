dac2ade4b34429b14488aa24a1f65b998daa8603
Matheus Jorge <mrljorge@outlook.com>
2024-05-17 13:28:54 -0300

    cleanup

---
+++ Top10/SetupView.swift
@@ -0,0 +1,18 @@
+//
+//  SetupView.swift
+//  Top10
+//
+//  Created by Matheus Jorge on 5/17/24.
+//
+
+import SwiftUI
+
+struct SetupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetupView()
+}
